a2defe04f1e3dae7d6be4bf161335b952623e042
Polynice <e2586018@cmaisonneuve.qc.ca>
2026-09-15 10:53:38 -0400

    Code des classes sujets et obs

---
+++ models/subject.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstactmethod
+
+class Sujet(ABC):
+
+    def __init__(self):
+        self._observateurs = []
+    
+    def abonner(self, observateur) -> None:
+        self._observateurs.append(observateur)
+
+    def desabonner(self, observateur) -> None:
+        self._observateurs.remove(observateur)
+
+    def notifier(self) -> None:
+        for ele in self._observateurs:
+            ele.actualiser(self)
+    
+    @abstractmethod
+    def get_donnees(self) -> dict:
+        pass

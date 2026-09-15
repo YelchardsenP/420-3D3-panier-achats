b4fa933b16de6d1e68c341cc4a99bf5f2fd126b5
Polynice <e2586018@cmaisonneuve.qc.ca>
2026-09-15 11:55:45 -0400

    ajout de debut code pou total et livraison

--- observers/affichage_total.py
+++ observers/affichage_total.py
@@ -4,7 +4,19 @@ from observers.observer import Observateur
 class AffichageTotal(Observateur):
     
     def __init__(self, parent):
-        pass
+        
+        self._label = tk.Label(
+            parent, 
+            text=f"Total : 0,00$", 
+            font=("Arial", 14, "bold"),
+            fg="gray"
+        )
+        self._label(pady=5)
     
     def actualiser(self, sujet) -> None:
-        pass
+
+        liste_articles = sujet.get_donnees()["articles"]
+
+        # Mettre à jour le total
+        total = sum(a['prix'] for a in self.panier)
+        self._label.config(text=f"Total : {total:2f}$")
--- observers/indicateur_livraison.py
+++ observers/indicateur_livraison.py
@@ -2,9 +2,26 @@ import tkinter as tk
 from observers.observer import Observateur
 
 class IndicateurLivraison(Observateur):
-    
+
     def __init__(self, parent):
-        pass
+        
+        self._label = tk.Label(
+            parent, 
+            text=f"Livraison... gratuite?", 
+            font=("Arial", 14, "bold"),
+            fg="gray"
+        )
+        self._label(pady=5)
     
     def actualiser(self, sujet) -> None:
-        pass
+
+        donnees = sujet.get_donnees()
+        
+        
+        self._label.config(text="Modification")
+    
+    
+        
+       
+    
+    
